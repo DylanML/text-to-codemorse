f0b51bf201a3f75f52779b932ab60e092b43cd9a
DylanML <dylan.moran.tics@gmail.com>
2023-06-24 20:49:59 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+morse_code = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', ' ': '/'
+}
+
+
+def convert_to_morse(text):
+    morse_text = ''
+    for char in text:
+        if char.upper() in morse_code:
+            morse_text += morse_code[char.upper()] + ' '
+        else:
+            morse_text += char
+    return morse_text
+
+
+word = input("Ingresa una palabra o frase: ")
+morse = convert_to_morse(word)
+print("Código Morse:", morse)
